2533408120f926b6ca4e970fca1fea70d3676e42
sairamyadhav <sairamyadhav4@gmail.com>
2025-10-07 12:49:49 +0530

    theme setup, hard coded as of now

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </StrictMode>,
+  </ThemeProvider>,
 )
